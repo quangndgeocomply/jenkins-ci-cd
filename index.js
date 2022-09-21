5fa093a23cc4bee7e5a720cf378a7de0b0cfa4e2
quangndgeocomply <109272469+quangndgeocomply@users.noreply.github.com>
2022-09-21 17:28:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const app = express();
 const port = 3001;
 app.listen(port, () => {
     console.log(`Listening to requests test 1 on http://localhost:${port}`);
+    console.log(`Listening to requests test 1 on http://localhost:${port}`);
   });
